8b5b65367b4049b81ec4ccb1f3a87ce0f6dc3e06
wanggang <wanggang@wanggangdeMacBook-Pro.local>
2021-06-12 01:18:22 +0800

    intall the UI controls

---
+++ main/components/category-grid/index.js
@@ -0,0 +1,23 @@
+// components/category-grid/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
