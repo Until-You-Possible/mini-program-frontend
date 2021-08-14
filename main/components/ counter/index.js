4420d4e4e57abb3f1e4ced845307a9f84aa823eb
untilYou <wanggang@until-you-possibles-MacBook-Pro.local>
2021-08-14 21:19:26 +0800

    对选择器逻辑的处理

--- main/components/ counter/index.js
+++ main/components/ counter/index.js
@@ -1,3 +1,5 @@
+import { Cart } from "../../model/cart";
+
 // components/ counter/index.js
 Component({
   /**
@@ -6,11 +8,15 @@ Component({
   properties: {
     count: {
       type: Number,
-      value: 1
+      value: Cart.SKU_MIN_COUNT
     },
     max: {
       type: Number,
-      value: 99
+      value: Cart.SKU_MAX_COUNT
+    },
+    min: {
+      type: Number,
+      value: Cart.SKU_MIN_COUNT
     }
   },
 
@@ -25,6 +31,22 @@ Component({
    * Component methods
    */
   methods: {
-
+    onOverStep (event) {
+      const minOrMaxOut = event.detail.type
+      if (minOrMaxOut === 'overflow_max') {
+          wx.showToast({
+              icon: "none",
+              duration: 3000,
+              title: `超出最大购买数量`
+          })
+      }
+      if (minOrMaxOut === 'overflow_min') {
+          wx.showToast({
+              icon: "none",
+              duration: 3000,
+              title: `最少需要购买${Cart.SKU_MIN_COUNT}件噢`
+          })
+      }
+    }
   }
 })
---
+++ main/model/cart.js
@@ -0,0 +1,9 @@
+
+class Cart {
+  static SKU_MIN_COUNT = 1;
+  static SKU_MAX_COUNT = 77
+}
+
+export {
+  Cart
+}
