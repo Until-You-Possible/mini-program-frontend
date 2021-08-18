3ae61a949ddc17539572e3af10474b571e76c059
untilYou <wanggang@until-you-possibles-MacBook-Pro.local>
2021-08-19 00:37:03 +0800

    增加search逻辑

---
+++ main/model/history-keyword.js
@@ -0,0 +1,38 @@
+
+
+class HistoryKeyword {
+  static MAX_ITEM_COUNT = 20;
+  static KEY = "keywords"
+  keyword = [];
+  get () {
+    return this.keyword;
+  }
+  constructor () {
+    this.keyword = this._getLocalKeywords();
+  }
+  save (keyword) {
+    const item = this.keyword.filter(v => v === keyword);
+    if (item.length !== 0 ) {
+      return;
+    }
+    if (item.length >= HistoryKeyword.MAX_ITEM_COUNT) {
+      this.keyword.pop();
+    }
+    this.keyword.unshift(keyword);
+    this._refreshLocal();
+  }
+  clear () {
+    this.keyword = [];
+    this._refreshLocal();
+  }
+  _refreshLocal () {
+    wx.setStorageSync(HistoryKeyword.KEY, this.keyword);
+  }
+  _getLocalKeywords () {
+    const results =  wx.getStorageSync(HistoryKeyword.KEY);
+    if (!results) {
+      wx.setStorageSync(HistoryKeyword.KEY, []);
+      return [];
+    }
+  }
+}
