0687ffd59eb471e77c2d7ebc57ba9657619c2da9
Gang Wang <gang.wang05@sap.com>
2021-08-18 17:18:49 +0800

    增加部分枚举的类型

--- main/core/enum.js
+++ main/core/enum.js
@@ -10,7 +10,77 @@ const ShoppingWay = {
   "BUY": "Buy"
 }
 
+const CouponCenterType = {
+  ACTIVITY: 'activity',
+  SPU_CATEGORY: 'spu_category'
+}
+
+const SpuListType = {
+  THEME: 'theme',
+  ROOT_CATEGORY: 'root_category',
+  SUB_CATEGORY: 'sub_category',
+  LATEST: 'latest'
+}
+
+const CouponType = {
+  FULL_MINUS: 1,
+  FULL_OFF: 2,
+  NO_THRESHOLD_MINUS: 3
+}
+
+const AuthAddress = {
+  DENY: 'deny',
+  NOT_AUTH: 'not_auth',
+  AUTHORIZED: 'authorized'
+}
+
+const CouponOperate = {
+  PICK: 'pick',
+  UNPICK: 'unpick'
+}
+
+
+const OrderStatus = {
+  ALL: 0,
+  UNPAID: 1,
+  PAID: 2,
+  DELIVERED: 3,
+  FINISHED: 4,
+  CANCELED: 5,
+}
+
+const BannerItemType = {
+SPU: 1,
+THEME: 2,
+SPU_LIST: 3
+}
+
+const OrderExceptionType = {
+  BEYOND_STOCK: 'beyond_stock',
+  BEYOND_SKU_MAX_COUNT: 'beyond_sku_max_count',
+  BEYOND_ITEM_MAX_COUNT: 'beyond_item_max_count',
+  SOLD_OUT: 'sold_out',
+  NOT_ON_SALE: 'not_on_sale',
+  EMPTY: 'empty'
+}
+
+const CouponStatus = {
+  CAN_COLLECT: 0,
+  AVAILABLE: 1,
+  USED: 2,
+  EXPIRED: 3
+}
+
 export {
   CellStatus,
-  ShoppingWay
+  ShoppingWay,
+  SpuListType,
+  AuthAddress,
+  OrderExceptionType,
+  CouponCenterType,
+  CouponStatus,
+  CouponType,
+  CouponOperate,
+  OrderStatus,
+  BannerItemType
 }
